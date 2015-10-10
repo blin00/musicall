955263f0b6b6d776035663597419c0599434ae01
Zhongxia Yan <zxyan@berkeley.edu>
2015-10-10 11:10:19 -0700

    Fixed accidental deletion of part of Brandon's code and added Bluetooth connection stuff. Can't test it but can currently receive and send connection I think

--- app/src/main/java/com/megabit/musicall/BluetoothConnection.java
+++ app/src/main/java/com/megabit/musicall/BluetoothConnection.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class BluetoothConnection {
     private BluetoothManager mBluetoothManager;
@@ -23,10 +30,112 @@ public class BluetoothConnection {
     Activity mCurrActivity;
 
     ArrayAdapter<String> mArrayAdapter = null;
+    ArrayList<BluetoothDevice> mDetectedDevices = null;
+    BluetoothDevice mSelectedDevice = null;
 
     private final static int REQUEST_ENABLE_BT = 43;
     private static final int BT_SCAN_PERIOD = 10000;
     private static final int DISCOVERABLE_DURATION = 300;
+    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    private class BTAcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public BTAcceptThread() {
+            // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("BT_SERVER", uuid);
+            } catch (IOException e) { }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection was accepted
+                if (socket != null) {
+                    // Do work to manage the connection (in a separate thread)
+                    manageConnectedSocket(socket);
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+
+        /** Will cancel the listening socket, and cause the thread to finish */
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private class BTConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public BTConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(uuid);
+            } catch (IOException e) { }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) { }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            manageConnectedSocket(mmSocket);
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private class ListClickListener implements AdapterView.OnItemClickListener {
+        public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+            System.out.println(position);
+            mSelectedDevice = mDetectedDevices.get(position);
+            BTConnect();
+        }
+    }
 
     public BluetoothConnection(Activity activity) {
         mCurrActivity = activity;
@@ -46,7 +155,7 @@ public class BluetoothConnection {
             mCurrActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
         mArrayAdapter = new ArrayAdapter<String>(mCurrActivity, android.R.layout.simple_list_item_1);
-
+        mDetectedDevices = new ArrayList<>();
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         // If there are paired devices
         if (pairedDevices.size() > 0) {
@@ -54,16 +163,22 @@ public class BluetoothConnection {
             for (BluetoothDevice device : pairedDevices) {
                 // Add the name and address to an array adapter to show in a ListView
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                mDetectedDevices.add(device);
             }
         }
         final ListView listview = (ListView) mCurrActivity.findViewById(R.id.listview);
-        System.out.println(mArrayAdapter.getCount());
         listview.setAdapter(mArrayAdapter);
+        listview.setOnItemClickListener(new ListClickListener());
         mBluetoothAdapter.startDiscovery();
 
         // Register the BroadcastReceiver
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         mCurrActivity.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+    }
+
+    public void BTConnect() {
+        BTConnectThread btThread = new BTConnectThread(mSelectedDevice);
+        btThread.run();
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND
@@ -76,9 +191,15 @@ public class BluetoothConnection {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // Add the name and address to an array adapter to show in a ListView
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                mDetectedDevices.add(device);
             }
         }
     };
+
+    private void manageConnectedSocket(BluetoothSocket socket) {
+        //TODO
+        //close socket when done
+    }
 
     /**
      * Attempts to connect to other device within BT_SCAN_PERIOD
--- app/src/main/java/com/megabit/musicall/MainActivity.java
+++ app/src/main/java/com/megabit/musicall/MainActivity.java
@@ -6,11 +6,13 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -31,12 +33,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performFileSearch();
+            }
+        });
         mediaPlayer = new MediaPlayer();
 
         btConn = new BluetoothConnection(this);
         btConn.discoverDevices();
-        btConn.receiveDiscovery();
+//        btConn.receiveDiscovery();
     }
 
     @Override
